5b3703bf4a33976d444e46983c8558fffb3fd1ce
Влад Сологуб <143345251+vladsologyb@users.noreply.github.com>
2024-05-31 23:23:00 +0300

    start hw 5

--- src/articles-api.js
+++ src/articles-api.js
@@ -1,7 +1,7 @@
 import  axios  from "axios";
 
 const API_KEY = "TiX6_SucpFpj3g4kTAJkSz-JJMEsg5hV3VUf7B5f1s0"
-axios.defaults.baseURL = "https://api.unsplash.com";
+axios.defaults.baseURL = 'https://api.themoviedb.org';
 
 export const fetchArticles = async (query, currentPage) => {
     const response = await axios.get("/search/photos?",{
